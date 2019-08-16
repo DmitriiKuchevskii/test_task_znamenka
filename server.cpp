44b1a7843d363c0d3706ffedd21c41e97d84a2dd
Dmitrii Kuchevskii <dmitrii@kuchevskii.com>
2019-08-16 19:56:25 +0300

    Added file name parameter for server

--- server.cpp
+++ server.cpp
@@ -1,10 +1,20 @@
 #include "server.hpp"
 
-int main()
+int main(int argc, char** argv)
 {
     try
     {
-        Server::create("test_file.txt")->run();
+        if (argc < 2)
+        {
+            std::cout << "You must provide file name for broadcasting\n";
+            exit(EXIT_FAILURE);
+        }
+        if (!std::ifstream(argv[1], std::ios::binary).good())
+        {
+            std::cout << "File " << argv[1] << "does not exist\n";
+            exit(EXIT_FAILURE);
+        }
+        Server::create(argv[0])->run();
     }
     catch(const std::exception& e)
     {
